cafdd3bfcafbb422af88ee2d77772bee2452659b
marcotristani <marco.tristani@hotmail.it>
2025-12-01 14:43:11 +0100

    flat: logica dell'esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 //COSTO BIGLIETTO TRENO
+
+//INPUT
+//Chiedere a utente di unserire chilometri da percorrere e età
+
+//ELABORAZIONE
+//verificare i dati in entrata se sono corretti
+//calcolare prezzo totale del biglietto 0.21£*km da percorrere
+//se minori di 18 anni il rezzo totale del biglietto va scontato del 20%
+//se maggiori di 65 anni il prezzo totale del biglietto va scontato del 40%
+//Trasformare il prezzo del biglietto in forma umana(massimo due decimali)
+
+//OUTPUT
