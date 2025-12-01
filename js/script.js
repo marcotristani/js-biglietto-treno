c82b81f5cd4c1181de6d113ee41a4d9737db3bf3
marcotristani <marco.tristani@hotmail.it>
2025-12-01 15:43:04 +0100

    flat: output in pagina html

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 const kmtogo = parseInt( prompt( "Inserire km da percorrere" ) );
 const ageUser = parseInt( prompt( "Inserire età del passeggero" ) );
 console.log("Km da percorrere:", kmtogo, "Età passeggero:", ageUser );
-
+let priceTiketReduced = "Nessuna riduzione per la vostra età"
 //ELABORAZIONE
 //verificare i dati in entrata se sono corretti
 if ( Number.isNaN(kmtogo) || Number.isNaN(ageUser) ) {
@@ -16,17 +16,16 @@ let priceTiket = kmtogo * (0.21) ;
 console.log("prezzo intero:", priceTiket );
 //se minori di 18 anni il rezzo totale del biglietto va scontato del 20%
 if ( ageUser < 18 ) {
-    priceTiket = priceTiket - ( ( priceTiket * 20 ) / 100 );
-    priceTiket = priceTiket.toFixed(2);//Trasformare il prezzo del biglietto in forma umana(massimo due decimali)
-    console.log( "prezzo ridotto per minorenni:", priceTiket);//output console
+    priceTiketReduced = priceTiket - ( ( priceTiket * 20 ) / 100 );
+    priceTiketReduced = priceTiketReduced.toFixed(2);//Trasformare il prezzo del biglietto in forma umana(massimo due decimali)
+    console.log( "prezzo ridotto per minorenni:", priceTiketReduced);//output console
     //se maggiori di 65 anni il prezzo totale del biglietto va scontato del 40%
 } else if ( ageUser >= 65 ) {
-    priceTiket = priceTiket - ( (priceTiket * 40 ) / 100 );
-    priceTiket = priceTiket.toFixed(2);//Trasformare il prezzo del biglietto in forma umana(massimo due decimali)
-    console.log( "prezzo ridotto per gli over 65:", priceTiket);//output console
+    priceTiketReduced = priceTiket - ( (priceTiket * 40 ) / 100 );
+    priceTiketReduced = priceTiketReduced.toFixed(2);//Trasformare il prezzo del biglietto in forma umana(massimo due decimali)
+    console.log( "prezzo ridotto per gli over 65:", priceTiketReduced);//output console
 }
 
-
-
-
 //OUTPUT
+document.getElementById("priceTiket").innerHTML = priceTiket;
+document.getElementById("priceTiketReduced").innerHTML = priceTiketReduced;
