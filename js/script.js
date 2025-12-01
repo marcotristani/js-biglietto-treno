0b1024176f2e72fe0c45c9e7e359a253c89d78f0
marcotristani <marco.tristani@hotmail.it>
2025-12-01 15:02:16 +0100

    flat: definite e verificate variabili di input

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 
 //INPUT
 //Chiedere a utente di unserire chilometri da percorrere e età
+const kmtogo = parseInt( prompt( "Inserire km da percorrere" ) );
+const ageUser = parseInt( prompt( "Inserire età del passeggero" ) );
+console.log("Km da percorrere:", kmtogo, "Età passeggero:", ageUser );
 
 //ELABORAZIONE
 //verificare i dati in entrata se sono corretti
+if ( Number.isNaN(kmtogo) || Number.isNaN(ageUser) ) {
+    alert( "Reinserire i dati,ricaricare la pagina" );
+}
 //calcolare prezzo totale del biglietto 0.21£*km da percorrere
+
 //se minori di 18 anni il rezzo totale del biglietto va scontato del 20%
 //se maggiori di 65 anni il prezzo totale del biglietto va scontato del 40%
 //Trasformare il prezzo del biglietto in forma umana(massimo due decimali)
