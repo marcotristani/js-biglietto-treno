9bf7902f2accb8d81f2e7cb6e62c773ebbbc9ff5
marcotristani <marco.tristani@hotmail.it>
2025-12-01 16:02:27 +0100

    flat: sistemato output

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 //INPUT
 //Chiedere a utente di unserire chilometri da percorrere e età
-const kmtogo = parseInt( prompt( "Inserire km da percorrere" ) );
-const ageUser = parseInt( prompt( "Inserire età del passeggero" ) );
+const kmtogo = parseFloat( prompt( "Inserire km da percorrere" ) );// usato parseFloat perchè possiamo accettari valori in km  con la virgola
+const ageUser = parseInt( prompt( "Inserire età del passeggero" ) );// usato parceInt perchè possiamo accettare solo vaoli interi di età
 console.log("Km da percorrere:", kmtogo, "Età passeggero:", ageUser );
 let priceTiketReduced = "Nessuna riduzione per la vostra età"
 //ELABORAZIONE
@@ -21,11 +21,12 @@ if ( ageUser < 18 ) {
     console.log( "prezzo ridotto per minorenni:", priceTiketReduced);//output console
     //se maggiori di 65 anni il prezzo totale del biglietto va scontato del 40%
 } else if ( ageUser >= 65 ) {
-    priceTiketReduced = priceTiket - ( (priceTiket * 40 ) / 100 );
-    priceTiketReduced = priceTiketReduced.toFixed(2);//Trasformare il prezzo del biglietto in forma umana(massimo due decimali)
+    priceTiketReduced = Math.round( ( priceTiket - ( (priceTiket * 40 ) / 100 ) ) *100 ) / 100; //Trasformare il prezzo del biglietto in forma umana(massimo due decimali)
     console.log( "prezzo ridotto per gli over 65:", priceTiketReduced);//output console
 }
 
 //OUTPUT
 document.getElementById("priceTiket").innerHTML = priceTiket;
 document.getElementById("priceTiketReduced").innerHTML = priceTiketReduced;
+document.getElementById("kmtogo").innerHTML = kmtogo.toString();
+document.getElementById("ageUser").innerHTML = ageUser.toString();
